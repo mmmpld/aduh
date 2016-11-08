09e9e27d447b64b54f8df47c42bb189f99fa709e
mmmpld <richardmolloy@gmail.com>
2016-11-09 08:41:57 +1300

    generic redis overload save for config and memory

--- aduh.py
+++ aduh.py
@@ -9,13 +9,20 @@ print("setting hangouts cookies.json to " + gauth)
 with open(os.path.join(sys.path[0], "hangupsbot/config/cookies.json"), "w") as text_file:
     text_file.write(gauth)
 
-print("creating config.json")
 r = redis.from_url(os.environ.get("REDIS_URL"))
-gotten_config = r.get('hangoutsbot_config')
+print("creating config.json")
+gotten_config = r.get('config.json')
 gotten_config_str = str(gotten_config,'utf-8')
 print('set config to: ' + gotten_config_str)
 with open(os.path.join(sys.path[0], "hangupsbot/config/config.json"), "w") as text_file:
     text_file.write(gotten_config_str)
 
+print("creating memory.json")
+gotten_memory = r.get('memory.json')
+gotten_memory_str = str(gotten_memory,'utf-8')
+print('set memory to: ' + gotten_memory_str)
+with open(os.path.join(sys.path[0], "hangupsbot/config/memory.json"), "w") as text_file:
+    text_file.write(gotten_memory_str)
+
 print("starting aduh hangouts")
-subprocess.call(["python", "hangupsbot.py", "-d", "--cookies", "config/cookies.json", "--config", "config/config.json"], cwd="hangupsbot")
+subprocess.call(["python", "hangupsbot.py", "-d", "--cookies", "config/cookies.json", "--config", "config/config.json", "--memory", "config/memory.json"], cwd="hangupsbot")
--- hangupsbot/plugins/redis.py
+++ hangupsbot/plugins/redis.py
@@ -10,6 +10,8 @@ import os
 import sys
 import redis
 import json
+import config
+from threading import Timer
 
 logger = logging.getLogger(__name__)
 
@@ -21,9 +23,10 @@ def _initialise(bot):
     global _bot
     _bot = bot
     plugins.register_admin_command(['testsave'])
-
-    _bot.config.save = save_redis
-
+    
+    # _bot.config.save = save_redis
+    _bot.config = RedisConfig(bot.config.filename)
+    _bot.memory = RedisConfig(bot.memory.filename)
 def save_redis():
     print('overloaded save called')
     #super(self).save()
@@ -39,5 +42,57 @@ def save_redis():
     r.set('hangoutsbot_config', hangoutsbot_config)
     print('saved config to redis')
 
+def redis_save(self, delay=True):
+    print('overloaded save')
+    #super(self).save()
+    # with open(_bot.config.filename, 'w') as f:
+    #     json.dump(_bot.config.config, f, indent=2, sort_keys=True)
+    #     _bot.changed = False
+    with open(self.filename, 'w') as f:
+        json.dump(self.config, f, indent=2, sort_keys=True)
+        self.changed = False
+    print('done file save')
+    print(self.filename)
+    # with open(_bot.config.filename), 'r') as config_file:
+    #     hangoutsbot_config = config_file.read()
+    #     print('read config from file')
+    r = redis.from_url(os.environ.get("REDIS_URL"))
+    r.set(os.path.basename(self.filename), self.config)
+    print('saved to redis')
+
 def testsave(bot, event):
+    print('test save called')
     bot.config.save()
+    bot.memory.save()
+
+class RedisConfig(config.Config):
+    def save(self, delay=True):
+        print('overloaded redis config save called')
+        print(os.path.basename(self.filename))
+        r = redis.from_url(os.environ.get("REDIS_URL"))
+        r.set(os.path.basename(self.filename), json.dumps(self.config, sort_keys=True))
+        print('saved to redis')
+
+        if self.save_delay:
+            if delay:
+                if self._timer_save and self._timer_save.is_alive():
+                    self._timer_save.cancel()
+                self._timer_save = Timer(self.save_delay, self.save, [], {"delay": False})
+                self._timer_save.start()
+                return False
+
+        """Save config to file (only if config has changed)"""
+        if self.changed:
+            start_time = time.time()
+
+            if self.failsafe_backups:
+                self._make_failsafe_backup()
+
+            with open(self.filename, 'w') as f:
+                json.dump(self.config, f, indent=2, sort_keys=True)
+                self.changed = False
+            interval = time.time() - start_time
+
+            logger.info("{} write {}".format(self.filename, interval))
+
+        return self.changed
