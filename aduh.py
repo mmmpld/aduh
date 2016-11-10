5e09b19cd1cba7b44e94ab756d2c13b9e25b63b8
mmmpld <richardmolloy@gmail.com>
2016-11-10 20:01:38 +1300

    less debug messages

--- aduh.py
+++ aduh.py
@@ -10,10 +10,10 @@ with open(os.path.join(sys.path[0], "hangupsbot/config/cookies.json"), "w") as t
     text_file.write(gauth)
 
 r = redis.from_url(os.environ.get("REDIS_URL"))
-#print("creating config.json")
+print("creating config.json")
 gotten_config = r.get('config.json')
 gotten_config_str = str(gotten_config,'utf-8')
-print('set config to: ' + gotten_config_str)
+#print('set config to: ' + gotten_config_str)
 with open(os.path.join(sys.path[0], "hangupsbot/config/config.json"), "w") as text_file:
     text_file.write(gotten_config_str)
 
